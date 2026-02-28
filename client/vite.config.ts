4c2ca9a72236c470666f4df278127682b8f14826
Oliver Leisalu <balduran123@gmail.com>
2026-02-28 17:10:35 +0200

    init

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
         port: 3000,
         strictPort: true,
         host: '0.0.0.0',
-        allowedHosts: 'all',
+        allowedHosts: true,
         proxy: {
             '/api': 'http://localhost:9000',
         },
